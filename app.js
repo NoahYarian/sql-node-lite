58783319f6b51d0abf8b7d79fecdf5239fd9c3ab
Noah Yarian <noah.yarian@gmail.com>
2015-09-01 11:09:44 -0500

    added some more queries

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 var sqlite3  = require('sqlite3');
 var db = new sqlite3.Database('./Northwind.sl3');
 
-db.each('SELECT * FROM Categories', function (err, row) {
-  console.log(row);
-});
+getCategories(getProducts);
+
+function getCategories(cb) {
+  console.log('/=========\\');
+  console.log('|Catgories|');
+  console.log('\\=========/');
+
+  db.each('SELECT * FROM Categories', function (err, row) {
+    console.log(row.Description.toString());
+  }, cb);
+}
+
+function getProducts() {
+  console.log('/========\\');
+  console.log('|Products|');
+  console.log('\\========/');
+
+  db.each('SELECT * FROM Products ' +
+    'INNER JOIN Categories ' +
+    'ON Products.CategoryID = Categories.CategoryID ' +
+    'LIMIT 10', function (err, row) {
+      console.log(row.ProductName + ' is a ' + row.CategoryName);
+    }, getEmployeeSupers);
+}
+
+function getEmployeeSupers() {
+  console.log('/====================\\');
+  console.log('|Employee Supervisors|');
+  console.log('\\====================/');
+
+  db.each('SELECT F.LastName AS "Employee", S.LastName AS "Supervisor" ' +
+    'FROM Employees F ' +
+    'INNER JOIN Employees S ' +
+    'ON F.ReportsTo = S.EmployeeID', function (err, row) {
+      console.log(row.Employee + "'s supervisor is " + row.Supervisor);
+    });
+}
 
 db.close();
