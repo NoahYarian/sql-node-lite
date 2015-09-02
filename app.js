f708249d6e0d25c3386b8318a0320560c1d2dab4
Noah Yarian <noah.yarian@gmail.com>
2015-09-02 15:30:07 -0500

    replaced node-sqlite with node-postgres, refactored first example problem

--- app.js
+++ app.js
@@ -1,191 +1,229 @@
-var sqlite3  = require('sqlite3');
-var db = new sqlite3.Database('./Northwind.sl3');
+var async = require('async');
+var pg = require('pg');
 
-db.serialize(function() {
+var conString = "postgres://localhost/Northwind";
 
-  db.run('', function () {
-    console.log('/=========\\');
-    console.log('|Categories|');
-    console.log('\\=========/');
-  });
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
 
-  getCategories();
-
-  db.run('', function () {
-    console.log('/========\\');
-    console.log('|Products|');
-    console.log('\\========/');
-  });
-
-  getProducts();
-
-  db.run('', function () {
-    console.log('/====================\\');
-    console.log('|Employee Supervisors|');
-    console.log('\\====================/');
-  });
-
-  getEmployeeSupers();
-
-  db.run('', function () {
-    console.log('/===============\\');
-    console.log('|createCFTable()|');
-    console.log('\\===============/');
-  });
-
-  createCFTable();
-
-  db.run('', function () {
-    console.log('/==============\\');
-    console.log('|insertCFRows()|');
-    console.log('\\==============/');
-  });
-
-  insertCFRows();
-
-  db.run('', function () {
-    console.log('/=====================\\');
-    console.log('|queryCFDescriptions()|');
-    console.log('\\=====================/');
-  });
-
-  queryCFDescriptions();
-
-  db.run('', function () {
-    console.log('/==================\\');
-    console.log('|updateCFTableRow()|');
-    console.log('\\==================/');
-  });
-
-  updateCFTableRow();
-
-  db.run('', function () {
-    console.log('/=====================\\');
-    console.log('|queryCFDescriptions()|');
-    console.log('\\=====================/');
-  });
-
-  queryCFDescriptions();
-
-  db.run('', function () {
-    console.log('/==================\\');
-    console.log('|deleteCFTableRow()|');
-    console.log('\\==================/');
-  });
-
-  deleteCFTableRow();
-
-  db.run('', function () {
-    console.log('/==============\\');
-    console.log('|insertCFRow2()|');
-    console.log('\\==============/');
-  });
-
-  insertCFRow2();
-
-  db.run('', function () {
-    console.log('/=====================\\');
-    console.log('|queryCFDescriptions()|');
-    console.log('\\=====================/');
-  });
-
-  queryCFDescriptions();
+  async.series([
+    function(callback) {
+      console.log('/==========\\');
+      console.log('|Categories|');
+      console.log('\\==========/');
+      callback(null, null);
+    },
+    function(callback) {
+      client.query('SELECT * FROM Categories;', function(err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+        result.rows.forEach(function(row) {
+          console.log(row.Description);
+        })
+      });
+      callback(null, null);
+    },
+    function() {
+      client.end();
+    }
+  ]);
 
 });
 
 
-function getCategories() {
+//////////
+// var sqlite3  = require('sqlite3');
+// var db = new sqlite3.Database('./Northwind.sl3');
 
-  db.each('SELECT * FROM Categories', function (err, row) {
-    console.log(row.Description.toString());
-  });
-}
+// db.serialize(function() {
 
-function getProducts() {
+//   db.run('', function () {
+//     console.log('/=========\\');
+//     console.log('|Categories|');
+//     console.log('\\=========/');
+//   });
 
-  db.each('SELECT * FROM Products ' +
-    'INNER JOIN Categories ' +
-    'ON Products.CategoryID = Categories.CategoryID ' +
-    'LIMIT 10', function (err, row) {
-      console.log(row.ProductName + ' is a ' + row.CategoryName);
-    });
-}
+//   getCategories();
 
-function getEmployeeSupers() {
+//   db.run('', function () {
+//     console.log('/========\\');
+//     console.log('|Products|');
+//     console.log('\\========/');
+//   });
 
-  db.each(
-    'SELECT E.LastName AS Employee, S.LastName AS Supervisor ' +
-    'FROM Employees AS E ' +
-    'LEFT OUTER JOIN Employees AS S ' +
-    'ON E.ReportsTo = S.EmployeeID', function (err, row) {
-      if (row.Supervisor === null) {
-        console.log(row.Employee + " has no supervisor");
-      } else {
-        console.log(row.Employee + "'s supervisor is " + row.Supervisor);
-      }
-    });
-}
+//   getProducts();
 
-function createCFTable() {
-  db.each(
-    'CREATE TABLE IF NOT EXISTS CategoryFavorites(' +
-    'FavoriteID INTEGER NOT NULL PRIMARY KEY, ' +
-    'CategoryID INTEGER NOT NULL, ' +
-    'FOREIGN KEY(CategoryID) REFERENCES Categories(CategoryID));'
-    , function(err, row) {
-      console.log(err);
-    });
-}
+//   db.run('', function () {
+//     console.log('/====================\\');
+//     console.log('|Employee Supervisors|');
+//     console.log('\\====================/');
+//   });
 
-function insertCFRows() {
-  db.each(
-    'INSERT INTO CategoryFavorites(CategoryID) VALUES (2),(4),(6),(8);'
-    , function(err, row) {
-      console.log(err);
-    });
-}
+//   getEmployeeSupers();
 
-function queryCFTable() {
-  db.each(
-    'SELECT * FROM CategoryFavorites;'
-    , function(err, row) {
-      console.log(row);
-    });
-}
+//   db.run('', function () {
+//     console.log('/===============\\');
+//     console.log('|createCFTable()|');
+//     console.log('\\===============/');
+//   });
 
-function queryCFDescriptions() {
-  db.each(
-    'SELECT CategoryFavorites.FavoriteID, Categories.Description ' +
-    'FROM CategoryFavorites ' +
-    'INNER JOIN Categories ' +
-    'ON Categories.CategoryID = CategoryFavorites.CategoryID;'
-    , function(err, row) {
-      console.log(row.FavoriteID + ": " + row.Description);
-    });
-}
+//   createCFTable();
 
-function updateCFTableRow() {
-  db.each(
-    'UPDATE CategoryFavorites SET CategoryID = 5 WHERE CategoryID = 4;'
-    , function(err, row) {
-      console.log(err);
-    });
-}
+//   db.run('', function () {
+//     console.log('/==============\\');
+//     console.log('|insertCFRows()|');
+//     console.log('\\==============/');
+//   });
 
-function deleteCFTableRow() {
-  db.each(
-    'DELETE FROM CategoryFavorites WHERE FavoriteID = 3;'
-    , function(err, row) {
-      console.log(err);
-    });
-}
+//   insertCFRows();
 
-function insertCFRow2() {
-  db.each(
-    'INSERT INTO CategoryFavorites(CategoryID) VALUES (1);'
-    , function(err, row) {
-      console.log(err);
-    });
-}
+//   db.run('', function () {
+//     console.log('/=====================\\');
+//     console.log('|queryCFDescriptions()|');
+//     console.log('\\=====================/');
+//   });
 
-db.close();
+//   queryCFDescriptions();
+
+//   db.run('', function () {
+//     console.log('/==================\\');
+//     console.log('|updateCFTableRow()|');
+//     console.log('\\==================/');
+//   });
+
+//   updateCFTableRow();
+
+//   db.run('', function () {
+//     console.log('/=====================\\');
+//     console.log('|queryCFDescriptions()|');
+//     console.log('\\=====================/');
+//   });
+
+//   queryCFDescriptions();
+
+//   db.run('', function () {
+//     console.log('/==================\\');
+//     console.log('|deleteCFTableRow()|');
+//     console.log('\\==================/');
+//   });
+
+//   deleteCFTableRow();
+
+//   db.run('', function () {
+//     console.log('/==============\\');
+//     console.log('|insertCFRow2()|');
+//     console.log('\\==============/');
+//   });
+
+//   insertCFRow2();
+
+//   db.run('', function () {
+//     console.log('/=====================\\');
+//     console.log('|queryCFDescriptions()|');
+//     console.log('\\=====================/');
+//   });
+
+//   queryCFDescriptions();
+
+// });
+
+
+// function getCategories() {
+
+//   db.each('SELECT * FROM Categories', function (err, row) {
+//     console.log(row.Description.toString());
+//   });
+// }
+
+// function getProducts() {
+
+//   db.each('SELECT * FROM Products ' +
+//     'INNER JOIN Categories ' +
+//     'ON Products.CategoryID = Categories.CategoryID ' +
+//     'LIMIT 10', function (err, row) {
+//       console.log(row.ProductName + ' is a ' + row.CategoryName);
+//     });
+// }
+
+// function getEmployeeSupers() {
+
+//   db.each(
+//     'SELECT E.LastName AS Employee, S.LastName AS Supervisor ' +
+//     'FROM Employees AS E ' +
+//     'LEFT OUTER JOIN Employees AS S ' +
+//     'ON E.ReportsTo = S.EmployeeID', function (err, row) {
+//       if (row.Supervisor === null) {
+//         console.log(row.Employee + " has no supervisor");
+//       } else {
+//         console.log(row.Employee + "'s supervisor is " + row.Supervisor);
+//       }
+//     });
+// }
+
+// function createCFTable() {
+//   db.each(
+//     'CREATE TABLE IF NOT EXISTS CategoryFavorites(' +
+//     'FavoriteID INTEGER NOT NULL PRIMARY KEY, ' +
+//     'CategoryID INTEGER NOT NULL, ' +
+//     'FOREIGN KEY(CategoryID) REFERENCES Categories(CategoryID));'
+//     , function(err, row) {
+//       console.log(err);
+//     });
+// }
+
+// function insertCFRows() {
+//   db.each(
+//     'INSERT INTO CategoryFavorites(CategoryID) VALUES (2),(4),(6),(8);'
+//     , function(err, row) {
+//       console.log(err);
+//     });
+// }
+
+// function queryCFTable() {
+//   db.each(
+//     'SELECT * FROM CategoryFavorites;'
+//     , function(err, row) {
+//       console.log(row);
+//     });
+// }
+
+// function queryCFDescriptions() {
+//   db.each(
+//     'SELECT CategoryFavorites.FavoriteID, Categories.Description ' +
+//     'FROM CategoryFavorites ' +
+//     'INNER JOIN Categories ' +
+//     'ON Categories.CategoryID = CategoryFavorites.CategoryID;'
+//     , function(err, row) {
+//       console.log(row.FavoriteID + ": " + row.Description);
+//     });
+// }
+
+// function updateCFTableRow() {
+//   db.each(
+//     'UPDATE CategoryFavorites SET CategoryID = 5 WHERE CategoryID = 4;'
+//     , function(err, row) {
+//       console.log(err);
+//     });
+// }
+
+// function deleteCFTableRow() {
+//   db.each(
+//     'DELETE FROM CategoryFavorites WHERE FavoriteID = 3;'
+//     , function(err, row) {
+//       console.log(err);
+//     });
+// }
+
+// function insertCFRow2() {
+//   db.each(
+//     'INSERT INTO CategoryFavorites(CategoryID) VALUES (1);'
+//     , function(err, row) {
+//       console.log(err);
+//     });
+// }
+
+// db.close();
